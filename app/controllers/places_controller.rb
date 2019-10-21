c72a180c4a20264485af020aa6f96cd54ca927a3
jcgdxn <jcgdxn@hotmail.com>
2019-10-21 18:45:41 +0000

    L#26_finit

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create :edit :update]
+  before_action :authenticate_user!, only: [:new, :create :edit :update, :destroy]
 
   def index
 
@@ -42,11 +42,16 @@ class PlacesController < ApplicationController
 
       @place.update_attributes(place_params)
       redirect_to root_path
-      
+
   end
 
   def destroy
     @place = Place.find(params[:id])
+    
+      if @place.user != current_user
+        return render plain: 'Not Permitted!!!', status: :forbidden
+      end
+
     @place.destroy
     redirect_to root_path
   end   
