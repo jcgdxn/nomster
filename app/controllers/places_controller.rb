18e7ac952e601125fcb080727b1472307edf8724
jcgdxn <jcgdxn@hotmail.com>
2019-10-21 17:22:32 +0000

    L#23_finit

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -25,9 +25,15 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
   end
 
-def edit
-  @place = Place.find(params[:id])
-end
+  def edit
+    @place = Place.find(params[:id])
+  end
+
+  def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
+  end
 
   private
 
