bc6b931e46bc7b50496c92d66fc8ba740b703b86
jcgdxn <jcgdxn@hotmail.com>
2019-11-02 04:45:01 +0000

    L42_kinda

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,9 +1,11 @@
 class NotificationMailer < ApplicationMailer
 
-    default from: "no-reply@info.local"
+    default from: "jcgdxn@live.com"
 
-    def comment_added
-      mail(to: "jcgdxn@hotmail.com",
+    def comment_added(comment)
+      @place = comment.place
+      @place_owner = @place.user
+      mail(to: "hqcntrl@gmail.com",
         Subject: "A comment has been added to your place")
 
     end
